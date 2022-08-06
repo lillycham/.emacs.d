055ec29550b4872a83bde8195f6c0f38e4410863
Lilly <lilly@lillycat.tech>
2022-08-06 16:54:06 +0100

    feat: Add elcord

--- init.el
+++ init.el
@@ -12,6 +12,9 @@
 (use-package ayu-theme
   :config (load-theme 'ayu-grey t))
 
+(require 'elcord)
+(elcord-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

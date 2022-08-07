6f9054dff82672b318c8f77b56df8e667606e120
Lilly <lilly@lillycat.tech>
2022-08-07 01:22:32 +0100

    chore: update config

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;; This is only needed once, near the top of the file
 (eval-when-compile
   (require 'use-package))
@@ -12,9 +11,30 @@
 (use-package ayu-theme
   :config (load-theme 'ayu-grey t))
 
+;; enable discord rpc
 (require 'elcord)
 (elcord-mode)
 
+;; enable web mode for jsx
+(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+
+;; enable 80 col ruler
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+
+;; enable line numbers
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; use bar cursor
+(setq-default cursor-type 'bar)
+
+;; disable tilde file stuff
+(setq backup-directory-alist `(("." . "~/.emacs.bak")))
+
+;; Enable racket xp minor mode
+(require 'racket-xp)
+(add-hook 'racket-mode-hook #'racket-xp-mode)
+
+;; Custom
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -28,6 +48,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000919" :foreground "#c3c0bb" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Liga SFMono Nerd Font")))))
-
-(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+ '(default ((t (:inherit nil :stipple nil :background "#000919" :foreground "#c3c0bb" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Liga SFMono Nerd Font"))))
+ '(line-number ((t (:inherit (shadow default) :background "gray7"))))
+ '(line-number-current-line ((t (:foreground "VioletRed1" :inherit line-number)))))
